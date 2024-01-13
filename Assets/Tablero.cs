0a3a3188c613130bbae245e3326f4c825c6ef1d4
YadokBlack <kodaygames@gmail.com>
2024-01-13 12:18:01 +0100

    Definir posibles valores o estados que tendrá cada casilla

---
+++ Assets/Tablero.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tablero : MonoBehaviour
+{
+
+    enum Colores
+    {
+        Rosa,
+        Roja
+    }
+
+    enum Efectos
+    {
+        pasa,
+        bloqueado
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
