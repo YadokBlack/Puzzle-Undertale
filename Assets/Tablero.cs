07c98eb5212dd8117c061d7377fc5a11f14510b0
YadokBlack <kodaygames@gmail.com>
2024-01-15 15:09:13 +0100

    Añadir funcion MostrarTablero en Tablero.cs

--- Assets/Tablero.cs
+++ Assets/Tablero.cs
@@ -25,15 +25,28 @@ public class Tablero : MonoBehaviour
     }
 
     public GameObject objetoCasilla;
+    public int ancho = 12;
+    public int alto = 8;
 
     void Start()
     {
-
+        CrearTablero();
     }
 
     void Update()
     {
         
+    }
+
+    public void CrearTablero()
+    {
+        for (int i = 0; i < ancho; i++)
+        {
+            for (int j = 0; j < alto; j++)
+            {
+                CrearNuevaCasilla(i, j, Color.green);
+            }
+        }
     }
 
     public void CrearNuevaCasilla(float x, float y, Color color)
